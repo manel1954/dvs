9613a0fb2d86556afc154c2d04091172f8b6249e
Manel <ea3eiz@ea3eiz.com>
2023-09-18 23:37:22 +0200

    1

---
+++ config/activar_hblink.php
@@ -0,0 +1,56 @@
+<?php 
+session_start();
+
+
+exec("sudo systemctl stop hblink.service");
+exec("sudo systemctl stop hbmon.service");
+exec("sudo systemctl stop parrotservice");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="UTF-8"> 
+     <meta http-equiv="content-type" content="text/html">
+     <meta name="author" content="EA3EIZ">
+     <link rel="shortcut icon" href="imagenes/favicon.png" type="image/x-icon" />
+     <title>cambiando</title>
+     <meta http-equiv="refresh" content="2; url=../index_dvswitch_buster.php" />
+
+    <!-- CSS Bootstrap-->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="custom/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<style>
+    body{
+    background:#333;
+    }
+    .texto_centrado_blanco{
+    padding-top: 60px;
+    color: #FFFFFF;
+    font-size: 30px;
+    text-align: center;
+    }
+    .config_especial{
+    height: 200px;
+    background:#8000FF;
+    border-radius: 8px 8px 8px 8px;
+    }    
+</style>
+
+</head>
+<body>
+ <div class="container"> 
+ 	<br><br>
+        <div class="row">
+               <div class="col-md-6 col-md-offset-2 config_especial">    
+               <h1 class="texto_centrado_blanco">ACTIVANDO HBLINK</h1>
+               </div> 	                       
+        </div><!--  row -->
+</div><!--container-->
+
+<!-- JavaScript================================================== -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
