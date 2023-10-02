b6ef4e9145a44987aa7c4aefdbd8253349f83e0a
Manel <ea3eiz@ea3eiz.com>
2023-10-02 13:16:37 +0200

    Update index_dvswitch_info.php

--- index_dvswitch_info.php
+++ index_dvswitch_info.php
@@ -188,27 +188,8 @@ $mmdvmconfigfile = getMMDVMConfigFileContent();
 <button class="button link_especial" ><a href="/dvs/index_dvswitch_buster.php" class="btn btn-danger" style="color:#9af240;">VOLVER A LAST HEARD</a</buttton>
 <!-- </div> --> 
 
-<?php
-        $aa = exec('awk "NR==1{print;exit}" /var/www/html/dvs/config/estado-dvswitch-hblink.txt');
-        if ($aa=="DVSWITCH-HBLINK_ACTIVADO=OK"){ 
-?>
-<head> 
-<!-- <div> -->
-<button class="button link_verde_claro" ><a href="/dvs/config/desactivar_hblink.php" class="btn btn-danger" style="color:#000;">DESACTIVAR DVSWITCH & HBLINK</a</buttton>
-</div> 
-<button class="bt btn-link btn-sm" name="cerrar_BM" onclick="desconectar_bm()"><img src="imagenes/ON.png" width="90" alt=""/><a href="panel_control.php" ></a></button> 
-</head>
-        
-        <?php
-        }
-        else {
-        ?>
-<head> 
-<div>
-<button class="button link_rojo" ><a href="/dvs/config/activar_hblink.php" class="btn btn-danger" style="color:#FFF;">ACTIVAR DVSWITCH & HBLINK</a</buttton>
-</div>
-<button class="bt btn-link btn-sm" name="cerrar_BM" onclick="conectar_bm()"><img src="imagenes/OFF.png" width="90" alt=""/><a href="panel_control.php" ></a></button>
-</head>
+
+
 
 <?php
         }
