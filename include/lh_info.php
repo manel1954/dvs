dc339b3fd2a275dd4613d8e1e154d295dd414ebd
Manel <ea3eiz@ea3eiz.com>
2023-10-02 02:17:01 +0200

    1

--- include/lh_info.php
+++ include/lh_info.php
@@ -3,22 +3,23 @@ include_once dirname(dirname(__FILE__)).'/include/config.php';
 include_once dirname(dirname(__FILE__)).'/include/tools.php';        
 include_once dirname(dirname(__FILE__)).'/include/functions.php';    
 ?>
-<span style="font-weight: bold;font-size:14px;">Gateway Activity</span>
-<fieldset style="height:90px;box-shadow:0 0 10px #999;background-color:#000; width:640px;margin-top:10px;margin-left:0px;margin-right:0px;font-size:12px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+<!-- <span style="font-weight: bold;font-size:14px;">Gateway Activity</span>
+ -->
+ <fieldset style="height:110px;margin-top:100px;">
   <table style="margin-top:3px;">
-    <tr>
+   <!--  <tr>
       <th>Time (<?php echo date('T')?>)</th>
       <th>Mode</th>
       <th>Callsign</th>
 <?php
-    if (DISPLAYNAME == "YES" && file_exists(DMRIDDATPATH."/DMRIds.dat") && ! empty(DMRIDDATPATH."/DMRIds.dat")) { echo "<th>Name</th>"; }
+    //if (DISPLAYNAME == "YES" && file_exists(DMRIDDATPATH."/DMRIds.dat") && ! empty(DMRIDDATPATH."/DMRIds.dat")) { echo "<th>Name</th>"; }
 ?>
       <th>Target</th>
       <th>Src</th>
       <th>Dur(s)</th>
       <th>Loss</th>
       <th>BER</th>
-    </tr>
+    </tr> -->
 <?php
 $i = 0;
 for ($i = 0;  ($i <= 0); $i++) { //Last 20 calls
@@ -33,18 +34,24 @@ for ($i = 0;  ($i <= 0); $i++) { //Last 20 calls
                         $local_time = strftime('%H:%M:%S %b %d', $dt->getTimestamp());
 
 		echo"<tr>";
-		echo"<td align=\"left\">&nbsp;$local_time</td>";
-		echo"<td align=\"left\" style=\"color:green; font-weight:bold;height:60px;\">&nbsp;$listElem[1]</td>";
+		/* echo"<td align=\"left\">&nbsp;$local_time</td>"; */
+		echo"<td align=\"center\" style=\"color:green; font-weight:bold;height:60px;font-size:22px;\">&nbsp;$listElem[1]</td>";
 		if ((is_numeric($listElem[2]) || strpos($listElem[2], "openSPOT") !== FALSE) && (strlen($listElem[2])==7)) {
-		    echo "<td align=\"left\" style=\"color:#464646;\">&nbsp;<a href=\"https://database.radioid.net/database/view?id=$listElem[2]\" target=\"_blank\"><span style=\"color:#464646;font-weight:bold;height:60px;\">$listElem[2]</span></a></td>";
+		    echo "<td align=\"center\" style=\"color:#464646;background:#000;\">&nbsp;<a href=\"https://database.radioid.net/database/view?id=$listElem[2]\" target=\"_blank\"><span style=\"color:#464646;font-weight:bold;height:60px;\">$listElem[2]</span></a></td>";
 		} elseif (!preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $listElem[2])|| $listElem[2] == "N0CALL") {
- 	                       echo "<td align=\"left\" style=\"color:#464646;height:60px;\"><b>&nbsp;$listElem[2]</b></td>";
+ 	                       echo "<td align=\"center\" style=\"color:#464646;background:#000;height:60px;\"><b>&nbsp;$listElem[2]</b></td>";
 		} else {
 		    if (strpos($listElem[2],"-") > 0) { $listElem[2] = substr($listElem[2], 0, strpos($listElem[2],"-")); }
 			    if ( $listElem[3] && $listElem[3] != '    ' ) {
-			echo "<td align=\"left\">&nbsp;<a href=\"http://www.qrz.com/db/$listElem[2]\" target=\"_blank\"><b>$listElem[2]</b></a><span style=\"color:#464646;font-weight:bold;height:60px;\">/$listElem[3]</span></td>";
+			
+			// INDICATIVO ***********************************
+			
+echo "<td align=\"center\" >&nbsp;<a href=\"http://www.qrz.com/db/$listElem[2]\" target=\"_blank\"><b>$listElem[2]</b></a><span>/$listElem[3]</span></td>";
 		    } else {
-			echo "<td align=\"left\">&nbsp;<a href=\"http://www.qrz.com/db/$listElem[2]\" style=\"background:#f00;height:60px;font-size:30px\" target=\"_blank\"><b>$listElem[2]</b></a></td>";
+			
+			
+			// INDICATIVO tamaño indicativo y fonfo indicativo ***********************************
+echo "<td align=\"center\" style=\"background:#000;font-size:60px;height:100px;\" >&nbsp;<a href=\"http://www.qrz.com/db/$listElem[2]\"  target=\"_blank\"><b>$listElem[2]</b></a></td>";
 		    }
 		}
 		// Display NAME by DV8AWC
@@ -74,27 +81,27 @@ for ($i = 0;  ($i <= 0); $i++) { //Last 20 calls
 		}
 		if (strlen($listElem[4]) == 1) { $listElem[4] = str_pad($listElem[4], 8, " ", STR_PAD_LEFT); }
 		if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
-			echo "<td align=\"left\">&nbsp;<span style=\"color:#b5651d;font-weight:bold;\">$listElem[4]</span></td>";
+			echo "<td align=\"left\">&nbsp;<span style=\"color:#b5651d;font-weight:bold;font-size:23px;\">$listElem[4]</span></td>";
 		} else {
-			echo "<td align=\"left\">&nbsp;<span style=\"color:#b5651d;font-weight:bold;\">".str_replace(" ","&nbsp;", $listElem[4])."</span></td>";
+			echo "<td align=\"left\">&nbsp;<span style=\"color:#b5651d;font-weight:bold;font-size:23px;\">".str_replace(" ","&nbsp;", $listElem[4])."</span></td>";
 		}
 
 
-		if ($listElem[5] == "LNet"){
+/* 		if ($listElem[5] == "LNet"){
 			echo "<td style=\"background:#1d1;\">LNet</td>";
 		}else{
 			echo "<td>$listElem[5]</td>";
-		}
+		} */
 		if ($listElem[6] == null) {
-                             if ($listElem[1] == "DMR Slot 2" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#f93;\">&nbsp;&nbsp;&nbsp;RX DMR&nbsp;&nbsp;&nbsp;</td>";}
-                             if ($listElem[1] == "DMR Slot 1" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#f93;\">&nbsp;&nbsp;&nbsp;RX DMR&nbsp;&nbsp;&nbsp;</td>";}
+                             if ($listElem[1] == "DMR Slot 2" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#f00;font-size:22px;\">&nbsp;&nbsp;&nbsp;RX DMR&nbsp;&nbsp;&nbsp;</td>";}
+                             if ($listElem[1] == "DMR Slot 1" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#f00;font-size:22px;\">&nbsp;&nbsp;&nbsp;RX DMR&nbsp;&nbsp;&nbsp;</td>";}
                              if ($listElem[1] == "YSF" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#ff9;\">&nbsp;&nbsp;&nbsp;RX YSF&nbsp;&nbsp;&nbsp;</td>";}
                              if ($listElem[1] == "P25" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#f9f;\">&nbsp;&nbsp;&nbsp;RX P25&nbsp;&nbsp;&nbsp;</td>";}
 			     if ($listElem[1] == "NXDN" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#c9f;\">&nbsp;&nbsp;&nbsp;RX NXDN&nbsp;&nbsp;</td>";}
                              if ($listElem[1] == "D-Star" && $listElem[5] == "Net")  {echo "<td colspan=\"3\" style=\"background:#ade;\">&nbsp;&nbsp;&nbsp;RX D-Star</td>";}
-                             if ($listElem[5] == "LNet")  {echo "<td colspan=\"3\" style=\"background:#f33;\">TX</td>";}
+                             if ($listElem[5] == "LNet")  {echo "<td colspan=\"3\" style=\"background:#f00;font-size:20px\">TX</td>";}
 			} else if ($listElem[6] == "DMR Data") {
-				echo "<td colspan=\"3\" style=\"background:#1d1;\">DMR Data</td>";
+				echo "<td colspan=\"3\" style=\"background:#1d1;font-size:20px;\">DMR Data</td>";
 			} else if ($listElem[6] == "GPS") {
 				echo "<td colspan=\"3\" style=\"background:#1d1;\"><a style=\"display:block;\" target=\"_blank\" href=https://www.openstreetmap.org/?mlat=".floatval($listElem[9])."&mlon=".floatval($listElem[10])."><b>GPS</b></a></td>";
 			} else {
