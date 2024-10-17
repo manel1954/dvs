3b776a877b2d7b8d769a6c1c299547f1e2af79d3
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-17 21:55:02 +0200

    Update dentro.php

--- config/dentro.php
+++ config/dentro.php
@@ -4,9 +4,9 @@ $passIng = strip_tags(sha1($_POST['pass']));
 $mipass="6ab4d12f3f825d988004d61bbc9653a551fbc385";
 if ($passIng==$mipass){
 $_SESSION['permisos'] = 1;
-header("Location:/dvs/panel_configuracion.php");	
+header("Location:../../index.php");	
 }
 else{
-header("Location:/dvs/index.php");
+header("Location:../../dvs/index.php");
 }
 ?>
