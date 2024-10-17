684e5f7af45b8096f2b6c9517e0848ad5073c33c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-17 22:38:45 +0200

    1

--- config/prueba.php
+++ config/prueba.php
@@ -3,29 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceso a prueba.php</title>
-    <script>
-        function solicitarPassword() {
-            var password = prompt("Por favor, ingresa la contraseña:");
-            
-            // Define la contraseña correcta aquí
-            var passwordCorrecta = "12345";
-            
-            if (password === passwordCorrecta) {
-                // Redirige a la página prueba.php si la contraseña es correcta
-                window.location.href = "prueba.php";
-            } else {
-                // Muestra un mensaje de error si la contraseña es incorrecta
-                alert("Contraseña incorrecta. Inténtalo de nuevo.");
-            }
-        }
-    </script>
+    <title>Hola Mundo</title>
 </head>
 <body>
-    <h1>Bienvenido</h1>
-    <p>Haz clic en el botón para acceder a la página protegida:</p>
-    
-    <!-- Botón que solicita la contraseña -->
-    <button onclick="solicitarPassword()">Acceder a prueba.php</button>
+    <h1>Hola Mundo</h1>
 </body>
 </html>
+
