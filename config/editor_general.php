6228b7deb57dd39940703b19f8fb4619c949a23c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-17 20:19:56 +0200

    Update editor_general.php

--- config/editor_general.php
+++ config/editor_general.php
@@ -221,7 +221,7 @@ background-image: url(img/fondo_02.png);
     border-radius: 8px 8px 8px 8px;
     }
 .config_especial{
-    height: 650px;
+    height: 750px;
     background:#d9540c;
     border-radius: 8px 8px 8px 8px;
     }
