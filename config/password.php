3b143e21fb0aebd3799932fd9c03813a5da5e685
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-18 17:35:41 +0200

    Update password.php

--- config/password.php
+++ config/password.php
@@ -22,11 +22,11 @@
             position: fixed;
             top: 0;
             left: 0;
-            width: 400px;
+            width: 100%;
             height: 100%;
             background-color: rgba(0, 0, 0, 0.5);
             justify-content: center;
-            align-items: center;
+            align-items: left;
         }
 
         /* Estilo para el contenido del modal */
