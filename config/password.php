077b294abb0c1e710e95e589189598c1c7d497ad
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-18 17:33:51 +0200

    Update password.php

--- config/password.php
+++ config/password.php
@@ -22,7 +22,7 @@
             position: fixed;
             top: 0;
             left: 0;
-            width: 200px;
+            width: 300px;
             height: 100%;
             background-color: rgba(0, 0, 0, 0.5);
             justify-content: center;
