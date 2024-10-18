578f42df29b06d452ff3f07cbc44bdcb336c309d
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-18 22:58:36 +0200

    Update actualizando_reflectores.php

--- config/actualizando_reflectores.php
+++ config/actualizando_reflectores.php
@@ -6,7 +6,7 @@
      <meta name="author" content="EA3EIZ">
      <link rel="shortcut icon" href="imagenes/favicon.png" type="image/x-icon" />
      <title>cambiando</title>
-     <meta http-equiv="refresh" content="2; url=../index_dvswitch_buster.php" />
+     <meta http-equiv="refresh" content="2; url=../index_botones.php" />
 
     <!-- CSS Bootstrap-->
     <link href="css/bootstrap.css" rel="stylesheet">
