0b5f2bddaf75435d0bfdfdeca9d588ac4bd7cbf9
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-19 09:39:18 +0200

    Update editor_general.php

--- config/editor_general.php
+++ config/editor_general.php
@@ -282,11 +282,13 @@ h6{
 
 <form method="post" action="/dvs/config/cambia_puerto.php">    
     <div class="fondo_datos">Puerto USRP: 
-        <span class="color_verde"><?php echo $puerto;?>
-        <input name="puerto" class="fuente_boton3 form-control" placeholder="Introduce Puerto USRP + Enter"></span>
-
+        <span class="color_verde"><?php echo $puerto;?></span>       
     </div> 
-        
+    
+    <div>
+    <input name="puerto" class="fuente_boton3 form-control" placeholder="Introduce Puerto USRP + Enter">
+    </div>
+    
 </form>
 
 <br>
