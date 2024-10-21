a7d40173723471de5cd3535ea6b3dde922513eea
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-21 18:29:50 +0200

    Update editor_general.php

--- config/editor_general.php
+++ config/editor_general.php
@@ -306,14 +306,14 @@ h6{
 
 <form method="post" action="/dvs/config/cambia_configuracion_latitud.php">            
 <div class="fondo_datos" style="display: flex; justify-content: space-between; align-items: center;">
-        <span>&nbsp;&nbsp;LATITUDE: </span><span class="color_verde"style="margin-left: auto;"><?php echo $latitude;?></span>
+        <span>&nbsp;&nbsp;LATITUD: </span><span class="color_verde"style="margin-left: auto;"><?php echo $latitude;?></span>
         <input name="latitude" class="fuente_boton3 form-control" placeholder="Latitude  + Enter" style="margin-left: 2px; width: 200px;">
         </div> 
 </form>
 
 <form method="post" action="/dvs/config/cambia_configuracion_longitud.php">            
     <div class="fondo_datos" style="display: flex; justify-content: space-between; align-items: center;">
-        <span>&nbsp;&nbsp;LONGITUDE: </span><span class="color_verde"style="margin-left: auto;"><?php echo $longitude;?></span>
+        <span>&nbsp;&nbsp;LONGITUD </span><span class="color_verde"style="margin-left: auto;"><?php echo $longitude;?></span>
         <input name="longitude" class="fuente_boton3 form-control" placeholder="Longitude  + Enter" style="margin-left: 2px; width: 200px;">
     </div> 
 </form>
