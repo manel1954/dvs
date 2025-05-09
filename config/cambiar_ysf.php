4d4b581be8483c3be2cabcd19ad6f108b93052d6
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-05-09 17:49:45 +0200

    1

--- config/cambiar_ysf.php
+++ config/cambiar_ysf.php
@@ -1,5 +1,12 @@
 <?php 
 session_start();
 exec("/opt/MMDVM_Bridge/dvswitch.sh mode YSF");
+
+//RESTAURAR LOS SERVICIOS
+exec("sudo systemctl restart analog_bridge.service");
+exec("sudo systemctl restart md380-emu.service");
+exec("sudo systemctl restart mmdvm_bridge.service");
+exec("sudo systemctl restart nxdngatewayd.service");
+
 header("Location: ../index_dvswitch_buster.php");
 ?>
